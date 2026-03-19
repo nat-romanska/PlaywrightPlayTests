72f6e34049625a0e7debc8bab5b81a1566d02b0b
Natalia <nat.romanska@gmail.com>
2026-03-19 16:18:35 +0100

    Update registration page object

--- pages/cookiesConsent.page.ts
+++ pages/cookiesConsent.page.ts
@@ -1,9 +1,13 @@
 import { Locator, Page } from "@playwright/test";
 
 export class CookiesConsentPage {
-    readonly cookiesConsentButton: Locator;
+    private readonly cookiesConsentButton: Locator;
 
     constructor(page: Page) {
         this.cookiesConsentButton = page.getByRole('button', { name: 'Consent' });
     }
+
+    async accept() {
+        await this.cookiesConsentButton.click();
+      }
 }
---
+++ types.ts
@@ -0,0 +1,14 @@
+export type RegistrationData = {
+    password: string;
+    day: string;
+    month: string;
+    year: string;
+    name: string;
+    surname: string;
+    address: string;
+    country: string;
+    state: string;
+    city: string;
+    zipcode: string;
+    mobile: string;
+}
